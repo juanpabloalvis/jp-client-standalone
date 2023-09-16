8f314db953dbb9028d6e4a7dd326efbcaeb03aaf
Juan Pablo Alvis C <juanpabloalvis@gmail.com>
2023-09-16 12:40:10 -0500

    Initial commit

---
+++ src/main/java/com/jp/client/standalone/JpClientStandaloneApplication.java
@@ -0,0 +1,13 @@
+package com.jp.client.standalone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpClientStandaloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpClientStandaloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jp/client/standalone/JpClientStandaloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jp.client.standalone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpClientStandaloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
