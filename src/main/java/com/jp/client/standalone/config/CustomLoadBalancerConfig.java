4034676e949d41287723b96dc5126d3bf1fce496
Juan Pablo Alvis C <juanpabloalvis@gmail.com>
2023-09-18 15:55:19 -0500

    Call a service using discoveryClient

--- src/main/java/com/jp/client/standalone/config/CustomLoadBalancerConfig.java
+++ src/main/java/com/jp/client/standalone/config/CustomLoadBalancerConfig.java
@@ -6,7 +6,6 @@ import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.loadbalancer.core.ServiceInstanceListSupplier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
 
 import java.util.List;
