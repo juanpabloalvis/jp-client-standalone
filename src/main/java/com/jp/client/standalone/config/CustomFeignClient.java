9cbbf35d6fdf5c66685fcd169732314c2fa754a0
Juan Pablo Alvis C <juanpabloalvis@gmail.com>
2023-09-18 16:08:56 -0500

    Call a service using discoveryClient

--- src/main/java/com/jp/client/standalone/config/CustomFeignClient.java
+++ src/main/java/com/jp/client/standalone/config/CustomFeignClient.java
@@ -3,12 +3,11 @@ package com.jp.client.standalone.config;
 import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @FeignClient(name = "JP-CONFIG")
 @LoadBalancerClient(name = "JP-CONFIG", configuration = CustomLoadBalancerConfig.class)
 public interface CustomFeignClient {
-    @RequestMapping(method = RequestMethod.GET, value = "/application-name")
+    @GetMapping(value = "/application-name")
     ResponseEntity<String> getApp();
 }
